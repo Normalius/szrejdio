31a9a83a3a272b9cf68e0952078ffebc7ff928e7
Normalius <117549510+Normalius@users.noreply.github.com>
2024-03-13 22:14:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Funkcja pobierająca statystyki radia z serwera Shoutcast
+    // Funkcja pobierająca statystyki radia z zewnętrznego źródła
     function fetchRadioStats() {
-        // Dane z serwera Shoutcast (tymczasowo przykładowe dane)
-        const responseXML = `
-            <SHOUTCASTSERVER>
-                <STREAMSTATS>
-                    <TOTALSTREAMS>1</TOTALSTREAMS>
-                    <ACTIVESTREAMS>1</ACTIVESTREAMS>
-                    <CURRENTLISTENERS>1</CURRENTLISTENERS>
-                    <PEAKLISTENERS>2</PEAKLISTENERS>
-                    <MAXLISTENERS>50</MAXLISTENERS>
-                    <UNIQUELISTENERS>1</UNIQUELISTENERS>
-                    <AVERAGETIME>9</AVERAGETIME>
-                    <VERSION>2.6.1.777 (posix(linux x64))</VERSION>
-                    <STREAM id="1">
-                        <CURRENTLISTENERS>1</CURRENTLISTENERS>
-                        <PEAKLISTENERS>2</PEAKLISTENERS>
-                        <MAXLISTENERS>50</MAXLISTENERS>
-                        <UNIQUELISTENERS>1</UNIQUELISTENERS>
-                        <AVERAGETIME>9</AVERAGETIME>
-                        <SERVERGENRE>Misc</SERVERGENRE>
-                        <SERVERURL>https://normalius.github.io/szrejdio/</SERVERURL>
-                        <SERVERTITLE>Audycja</SERVERTITLE>
-                        <SONGTITLE>cute trance song catJAM</SONGTITLE>
-                        <DJ>Test</DJ>
-                        <STREAMHITS>44</STREAMHITS>
-                        <STREAMSTATUS>1</STREAMSTATUS>
-                        <BACKUPSTATUS>0</BACKUPSTATUS>
-                        <STREAMLISTED>0</STREAMLISTED>
-                        <STREAMLISTEDERROR>847993365</STREAMLISTEDERROR>
-                        <STREAMPATH>/</STREAMPATH>
-                        <STREAMUPTIME>362</STREAMUPTIME>
-                        <BITRATE>96</BITRATE>
-                        <SAMPLERATE>44100</SAMPLERATE>
-                        <CONTENT>audio/mpeg</CONTENT>
-                    </STREAM>
-                </STREAMSTATS>
-            </SHOUTCASTSERVER>
-        `;
+        // Adres URL do pobrania danych statystycznych
+        const url = "http://s3.slotex.pl:7466/statistics";
 
-        // Parsowanie XML
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(responseXML, "text/xml");
-        const stream = xmlDoc.querySelector("STREAM");
+        // Wywołanie żądania HTTP GET do serwera
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                // Parsowanie danych tekstowych do XML
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(data, "text/xml");
+                const stream = xmlDoc.querySelector("STREAM");
 
-        // Aktualizacja elementów na stronie z danymi statystycznymi
-        document.getElementById("radioName").innerText = stream.querySelector("SERVERTITLE").textContent;
-        document.getElementById("host").innerText = stream.querySelector("DJ").textContent;
-        document.getElementById("listeners").innerText = stream.querySelector("CURRENTLISTENERS").textContent;
-        document.getElementById("maxListeners").innerText = stream.querySelector("MAXLISTENERS").textContent;
-        document.getElementById("currentListeners").innerText = stream.querySelector("CURRENTLISTENERS").textContent;
+                // Aktualizacja elementów na stronie z danymi statystycznymi
+                document.getElementById("radioName").innerText = stream.querySelector("SERVERTITLE").textContent;
+                document.getElementById("host").innerText = stream.querySelector("DJ").textContent;
+                document.getElementById("listeners").innerText = stream.querySelector("CURRENTLISTENERS").textContent;
+                document.getElementById("maxListeners").innerText = stream.querySelector("MAXLISTENERS").textContent;
+                document.getElementById("currentListeners").innerText = stream.querySelector("CURRENTLISTENERS").textContent;
+            })
+            .catch(error => console.error("Błąd pobierania danych:", error));
     }
 
     // Wywołanie funkcji do pobrania statystyk po załadowaniu strony
