5c800dcc2beea2654b2afbe76714ecaa9ae79a22
Normalius <117549510+Normalius@users.noreply.github.com>
2024-03-13 22:43:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-// Plik JavaScript (script.js)
 document.addEventListener("DOMContentLoaded", function() {
     // Funkcja pobierająca statystyki radia z zewnętrznego źródła
     function fetchRadioStats() {
-        // Adres URL do pobrania danych statystycznych z PHP
-        const url = "http://s3.slotex.pl:7466/statistics";
+        // Adres URL do pobrania danych statystycznych z serwera Shoutcast2
+        const url = "http://s3.slotex.pl:7466/stats?sid=1";
 
         // Wywołanie żądania HTTP GET do serwera
         fetch(url)
-            .then(response => response.text())
+            .then(response => response.json()) // Parsowanie odpowiedzi jako JSON
             .then(data => {
-                // Parsowanie danych tekstowych do XML
-                const parser = new DOMParser();
-                const xmlDoc = parser.parseFromString(data, "text/xml");
-                const stream = xmlDoc.querySelector("STREAM");
-
                 // Aktualizacja elementów na stronie z danymi statystycznymi
-                document.getElementById("radioName").innerText = stream.querySelector("SERVERTITLE").textContent;
-                document.getElementById("host").innerText = stream.querySelector("DJ").textContent;
-                document.getElementById("listeners").innerText = stream.querySelector("CURRENTLISTENERS").textContent;
-                document.getElementById("maxListeners").innerText = stream.querySelector("MAXLISTENERS").textContent;
-                document.getElementById("currentListeners").innerText = stream.querySelector("CURRENTLISTENERS").textContent;
+                document.getElementById("radioName").innerText = data.icestats.source.server_name;
+                document.getElementById("host").innerText = data.icestats.source.listenurl;
+                document.getElementById("listeners").innerText = data.icestats.source.listeners;
+                document.getElementById("maxListeners").innerText = data.icestats.source.listener_peak;
+                document.getElementById("currentListeners").innerText = data.icestats.source.listeners;
             })
             .catch(error => console.error("Błąd pobierania danych:", error));
     }
