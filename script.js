d0826f24b6f68db3be76da53b34cc00e548bea73
Normalius <117549510+Normalius@users.noreply.github.com>
2024-03-14 14:39:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function updateRadioStats() {
 
 // Funkcja do pobierania danych statystycznych radia z serwera
 function fetchRadioStats() {
-    const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
     const shoutcastUrl = 'http://s3.slotex.pl:7466/'; // URL serwera Shoutcast
 
     // Wywołanie żądania HTTP GET za pośrednictwem CORS Anywhere
